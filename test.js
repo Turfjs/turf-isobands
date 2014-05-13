0d2cc257fe9ea76b26b05d979762efa0b8781614
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-05-13 16:03:23 -0400

    test passing for non donut isobands

--- test.js
+++ test.js
@@ -5,11 +5,11 @@ var isobands = require('./')
 test('isobands', function(t){
   var points = JSON.parse(fs.readFileSync(__dirname+'/geojson/Points.geojson'))
   
-  var isolined = isobands(points, 'elevation', 15, [25, 45, 55, 65, 85,  95, 105, 120, 180], false)
+  var isobanded = isobands(points, 'elevation', 15, [5, 45, 55, 65, 85,  95, 105, 120, 180], false)
 
-  t.ok(isolined.features, 'should take a set of points with z values and output a set of filled contour polygons')
-  t.equal(isolined.features[0].geometry.type, 'LineString')
+  t.ok(isobanded.features, 'should take a set of points with z values and output a set of filled contour polygons')
+  t.equal(isobanded.features[0].geometry.type, 'Polygon')
 
-  fs.writeFileSync(__dirname+'/geojson/isobands.geojson', JSON.stringify(isolined))
+  fs.writeFileSync(__dirname+'/geojson/isobands.geojson', JSON.stringify(isobanded))
   t.end()
 })
