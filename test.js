1bb45d9baa18f754b18a0f6facc7396fc60ecdfe
morganherlocker <morgan.herlocker@gmail.com>
2014-12-11 16:36:11 -0500

    add benching

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-var test = require('tape')
-var fs = require('fs')
-var isobands = require('./')
+var test = require('tape');
+var fs = require('fs');
+var isobands = require('./');
 
 test('isobands', function(t){
-  var points = JSON.parse(fs.readFileSync(__dirname+'/geojson/Points.geojson'))
+  var points = JSON.parse(fs.readFileSync(__dirname+'/geojson/Points.geojson'));
   
-  var isobanded = isobands(points, 'elevation', 15, [5, 45, 55, 65, 85,  95, 105, 120, 180], false)
+  var isobanded = isobands(points, 'elevation', 15, [5, 45, 55, 65, 85,  95, 105, 120, 180], false);
 
-  t.ok(isobanded.features, 'should take a set of points with z values and output a set of filled contour polygons')
-  t.equal(isobanded.features[0].geometry.type, 'Polygon')
+  t.ok(isobanded.features, 'should take a set of points with z values and output a set of filled contour polygons');
+  t.equal(isobanded.features[0].geometry.type, 'Polygon');
 
-  fs.writeFileSync(__dirname+'/geojson/isobands.geojson', JSON.stringify(isobanded))
-  t.end()
+  fs.writeFileSync(__dirname+'/geojson/isobands.geojson', JSON.stringify(isobanded));
+  t.end();
 })
